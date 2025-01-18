3f108e3001125fea22a44aa49ab3f451070ab636
bobo <187936623+bobo334@users.noreply.github.com>
2025-01-18 07:49:43 +0000

    Added new test

--- Test-INIReading-for-rdpwrap/Test-INIReading-for-rdpwrap.cpp
+++ Test-INIReading-for-rdpwrap/Test-INIReading-for-rdpwrap.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <windows.h>
 #include "INIParser.hpp"
 using namespace std;
 
@@ -13,27 +14,29 @@ int main()
             return 1;
         }
 
-        string section = "6.0.6000.16386";
+        string section = "10.0.22621.4655";
 
-        unsigned int singleUserOffsetX86 = parser.getDWordHex(section, "SingleUserOffset.x86", 0x0);
         unsigned int singleUserOffsetX64 = parser.getDWordHex(section, "SingleUserOffset.x64", 0x0);
-        unsigned int defPolicyOffsetX86 = parser.getDWordHex(section, "DefPolicyOffset.x86", 0x0);
         unsigned int defPolicyOffsetX64 = parser.getDWordHex(section, "DefPolicyOffset.x64", 0x0);
 
-        string singleUserCodeX86 = parser.getString(section, "SingleUserCode.x86", "");
-        string singleUserCodeX64 = parser.getString(section, "SingleUserCode.x64", "");
-        string defPolicyCodeX86 = parser.getString(section, "DefPolicyCode.x86", "");
+        if (parser.getBool(section, "SingleUserPatch.x64", "")) {
+            cout << "SingleUserPatch.x64 is ture" << endl;
+        }
+        else {
+            cout << "SingleUserPatch.x64 is false" << endl;
+        }
         string defPolicyCodeX64 = parser.getString(section, "DefPolicyCode.x64", "");
 
-        cout << "SingleUserOffset.x86: 0x" << hex << singleUserOffsetX86 << endl;
         cout << "SingleUserOffset.x64: 0x" << hex << singleUserOffsetX64 << endl;
-        cout << "DefPolicyOffset.x86: 0x" << hex << defPolicyOffsetX86 << endl;
         cout << "DefPolicyOffset.x64: 0x" << hex << defPolicyOffsetX64 << endl;
 
-        cout << "SingleUserCode.x86: " << singleUserCodeX86 << endl;
-        cout << "SingleUserCode.x64: " << singleUserCodeX64 << endl;
-        cout << "DefPolicyCode.x86: " << defPolicyCodeX86 << endl;
         cout << "DefPolicyCode.x64: " << defPolicyCodeX64 << endl;
+        stringstream ss;
+        ss << "10.0.22621.4655" << "-SLInit";
+        string section2 = ss.str();
+
+        DWORD bInitialized_x64 = parser.getDWordHex(section2, "bInitialized.x64", 0x0);
+        cout << "bInitialized.x64: 0x" << hex << bInitialized_x64 << endl;
 
     return 0;
 }
